d5e35d5c3f21b9cc08390d3d997539d5b5d733db
Mariusz Bąk <mariusz.alef.bak@gmail.com>
2018-06-11 18:30:46 +0200

    Linkify URLs in summary items. (#44)

---
+++ spa/src/app/pipes/linkify.pipe.ts
@@ -0,0 +1,15 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import htmlLinkify from 'html-linkify';
+
+const options = {
+    attributes: {
+        target: '_blank',
+    },
+};
+
+@Pipe({name: 'linkify'})
+export class LinkifyPipe implements PipeTransform {
+    transform(text: string): string {
+        return text ? htmlLinkify(text, options) : text;
+    }
+}

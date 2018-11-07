fb64e62e8fa38cbfbe32ba31c80a39cb3ea56ba0
Mariusz Bąk <mariusz.alef.bak@gmail.com>
2018-11-07 08:19:50 +0100

    Simplify API configuration. (#72) (#74)
    * Command logs in UI.
    
    * Config simplified.
    
    * Version number updated.

--- api/src/environments/environment.docker-dev.ts
+++ api/src/environments/environment.docker-dev.ts
@@ -5,10 +5,10 @@ export const environment: EnvironmentInterface = {
         versionNumber: '0.1.0',
     },
     mongo: {
-        dsn: 'mongodb://user:password@host:27017/feater',
+        dsn: 'mongodb://mongo:27017/feater',
     },
     redis: {
-        url: 'redis://user:password@host:6379/1',
+        url: 'redis://redis:6379/1',
     },
     guestPaths: {
         build: '/data/build/',
@@ -16,15 +16,15 @@ export const environment: EnvironmentInterface = {
         asset: '/data/asset/',
     },
     hostPaths: {
-        build: '/home/me/feater/data/build/',
-        asset: '/home/me/feater/data/asset/',
+        build: '/home/me/feater-data/build/',
+        asset: '/home/me/feater-data/asset/',
     },
     instantiation: {
         composeBinaryPath: '/usr/local/bin/docker-compose',
         composeHttpTimeout: 5000,
         dockerBinaryPath: '/usr/bin/docker',
         containerNamePrefix: 'featerinstance',
-        proxyDomainPattern: `{instance_hash}-{port_id}.feater.org`,
+        proxyDomainPattern: `{instance_hash}-{port_id}.my-feater-host`,
         proxyDomainsNetworkName: 'feater_featerproxy', // Value of COMPOSE_PROJECT_NAME is prepended by default.
     },
     logger: {

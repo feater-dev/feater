5f394be70fb6f2ebf5d9d061a8562b10bc1d834c
Mariusz Bąk <mariusz.alef.bak@gmail.com>
2018-11-09 09:55:45 +0100

    Polling restored in few components. (#78)

--- spa/src/app/instance/detail/summary/instance-detail-summary.component.ts
+++ spa/src/app/instance/detail/summary/instance-detail-summary.component.ts
@@ -6,15 +6,20 @@ import {
     GetInstanceDetailSummaryQueryInstanceFieldinterface,
     GetInstanceDetailSummaryQueryInterface,
 } from './get-instance-detail-summary.query';
+import {Observable, Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-instance-detail-summary',
     templateUrl: './instance-detail-summary.component.html',
     styles: []
 })
-export class InstanceDetailSummaryComponent implements OnInit {
+export class InstanceDetailSummaryComponent implements OnInit, OnDestroy {
+
+    readonly POLLING_INTERVAL = 5000; // 5 seconds.
 
     instance: GetInstanceDetailSummaryQueryInstanceFieldinterface;
+
+    pollingSubscription: Subscription;
 
     constructor(
         private route: ActivatedRoute,
@@ -23,6 +28,14 @@ export class InstanceDetailSummaryComponent implements OnInit {
 
     ngOnInit() {
         this.getInstance();
+        const polling = Observable.interval(this.POLLING_INTERVAL);
+        this.pollingSubscription = polling.subscribe(
+            () => { this.getInstance(); },
+        );
+    }
+
+    ngOnDestroy() {
+        this.pollingSubscription.unsubscribe();
     }
 
     private getInstance() {
@@ -32,6 +45,7 @@ export class InstanceDetailSummaryComponent implements OnInit {
                 variables: {
                     id: this.route.snapshot.params['id'],
                 },
+                fetchPolicy: 'network-only',
             })
             .valueChanges
             .subscribe(result => {

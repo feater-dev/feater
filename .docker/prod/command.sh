36f0f6f5cb82c7760e56fd3b70c1b302ad05babf
Mariusz Bąk <mariusz.alef.bak@gmail.com>
2019-03-03 04:49:34 +0100

    MongoDB added to image.

--- .docker/prod/command.sh
+++ .docker/prod/command.sh
@@ -5,6 +5,7 @@ set -ex
 docker pull alpine:3.9
 docker pull docker/compose:${FEATER_DOCKER_COMPOSE_VERSION}
 
-nginx -g "daemon on;" # TODO Move from here as it will vary depend on OS used.
+nginx -g "daemon on;" # TODO It may vary depending on OS used.
+mongod --fork --bind_ip 0.0.0.0 --logpath /var/log/mongodb.log --dbpath /data/mongo # TODO It may vary depending on OS used.
 
 pm2-runtime --name feater_server /app/server/main.js

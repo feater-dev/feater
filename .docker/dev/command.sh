f3bbbb466c3ee699dcf44befa6d21c70432f2eef
Mariusz Bąk <mariusz.alef.bak@gmail.com>
2019-03-03 04:51:03 +0100

    MongoDB added to image. (#112)

--- .docker/dev/command.sh
+++ .docker/dev/command.sh
@@ -5,14 +5,12 @@ set -ex
 docker pull alpine:3.9
 docker pull docker/compose:${FEATER_DOCKER_COMPOSE_VERSION}
 
-(cd ./server && yarn install)
-(cd ./client && yarn install)
+nginx -g "daemon on;" # TODO It may vary depending on OS used.
+mongod --fork --bind_ip 0.0.0.0 --logpath /var/log/mongodb.log --dbpath /data/mongo # TODO It may vary depending on OS used.
 
-nginx -g "daemon on;" # TODO Move from here as it will vary depend on OS used.
-
-(cd ./client && yarn start:dev:watch 1>&1 2>&2 &)
-(cd ./server && yarn start:dev:watch 1>&1 2>&2 &)
+(cd ./server && yarn install && yarn start:dev:watch 1>&1 2>&2 &)
+(cd ./client && yarn install && yarn start:dev:watch 1>&1 2>&2 &)
 
 set +x
 
-while true; do sleep 2; done
+while true; do sleep 10; done

882993074325c51599f7e64eb26242315ed94641
Mariusz Bąk <mariusz.alef.bak@gmail.com>
2019-03-06 21:00:41 +0100

    Cleanup of Docker related files. (#116)

--- .docker/build-dockerhub-images.sh
+++ .docker/build-dockerhub-images.sh
@@ -2,11 +2,7 @@
 
 set -ex
 
-image_tags=(
-    latest-docker-18.06.3
-    latest-docker-18.09.2
-    latest-docker-latest
-)
+version=latest
 
 docker_versions=(
     18.06.3
@@ -14,11 +10,11 @@ docker_versions=(
     latest
 )
 
-for i in "${!image_tags[@]}"; do
+for i in "${!docker_versions[@]}"; do
     docker build \
         --pull --no-cache \
         --build-arg DOCKER_VERSION=${docker_versions[$i]} \
         -f ./prod/Dockerfile \
-        -t feater:${image_tags[$i]} \
+        -t feater:${version}-docker-${docker_versions[$i]} \
         ..
 done
